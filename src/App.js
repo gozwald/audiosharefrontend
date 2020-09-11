29b15019cbf1e02584f904712787877fe40612cf
Samuel Greenwald <gozwald@gmail.com>
2020-09-11 20:28:14 +0200

    rudimentary recording works

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ReactMic } from "react-mic";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      record: false,
+    };
+  }
+
+  startRecording = () => {
+    this.setState({ record: true });
+  };
+
+  stopRecording = () => {
+    this.setState({ record: false });
+  };
+
+  onData(recordedBlob) {
+    console.log("chunk of real-time data is: ", recordedBlob);
+  }
+
+  onStop(recordedBlob) {
+    console.log("recordedBlob is: ", recordedBlob);
+  }
+
+  render() {
+    return (
+      <div>
+        <ReactMic
+          record={this.state.record}
+          className="sound-wave"
+          onStop={this.onStop}
+          onData={this.onData}
+          strokeColor="#000000"
+          backgroundColor="#FF4081"
+        />
+        <button onClick={this.startRecording} type="button">
+          Start
+        </button>
+        <button onClick={this.stopRecording} type="button">
+          Stop
+        </button>
+      </div>
+    );
+  }
 }
-
-export default App;
