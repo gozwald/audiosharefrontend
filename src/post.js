a770975fa728e0c016b9b8d26cda48d6d538e7d3
Samuel Greenwald <gozwald@gmail.com>
2020-09-19 23:11:44 +0200

    removed useEffect

--- src/post.js
+++ src/post.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { useVoiceRecorder } from "use-voice-recorder";
 import Cookies from "universal-cookie";
